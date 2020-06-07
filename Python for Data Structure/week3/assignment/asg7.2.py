aa47808e85d317cb538c39ec34dde30d00b320a2
sajal1302 <50777789+sajal1302@users.noreply.github.com>
2020-06-07 17:53:39 +0530

    2

---
+++ Python for Data Structure/week3/assignment/asg7.2.py
@@ -0,0 +1,14 @@
+count=0
+s=0
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") :
+        continue
+    else:
+        count=count+1
+        a=line.find('0')
+        x=line[a:]
+        s=s+float(x)
+avg=s/count
+print("Average spam confidence:",avg)
